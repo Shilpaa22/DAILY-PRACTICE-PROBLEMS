c104b2f67846bfc0aadbb4684293c59c39972a77
SHILPA MISHRA <shilpaaaaa2232@gmail.com>
2025-01-08 23:05:41 +0530

    Add files via upload

--- Task Scheduler.cpp
+++ Task Scheduler.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-       vector<int>freq(26,0);
-       for(int i=0;i<tasks.size();i++){
-        freq[tasks[i]-'A']++;
-       }
-       int maxi=0;
-       for(int i=0;i<26;i++){
-        maxi=max(maxi,freq[i]);
-       }
-       int count=0;
-       for(int i=0;i<26;i++){
-        if(freq[i]==maxi)count++;
-       }
-       int val=((maxi-1)*(n+1))+count;
-       int ans=max(val,(int)tasks.size());
-       return ans;
+
+        if(n==0) return tasks.size();
+
+        priority_queue<pair<int,char>> pq;
+        unordered_map<int,int> freq;
+
+        for(int i = 0;i<tasks.size();i++)
+        freq[tasks[i]]++;
+
+        for(auto x : freq)
+        pq.push({x.second,x.first});
+        
+        int ans = 0;
+        while(pq.size()>0)
+        {
+           if(pq.top().first==1)
+           {
+            ans+=pq.size();
+            break;
+           }
+           vector<pair<int,char>> v;
+           v.push_back(pq.top());
+           pq.pop();
+           ans+=1;
+           for(int i = 0;i<n;i++)
+           {
+               if(pq.size()>0)
+               {
+                 v.push_back(pq.top());
+                 pq.pop();
+               }
+               ans++;
+           }
+           for(auto x : v)
+           if(x.first>1) pq.push({x.first-1,x.second});
+        }
+        return ans; 
     }
 };
