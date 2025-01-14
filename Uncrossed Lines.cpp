96737ae890a2cae4feb5c5a168f0dda2d3ae97f7
SHILPA MISHRA <shilpaaaaa2232@gmail.com>
2025-01-14 09:45:21 +0530

    Add files via upload

--- Uncrossed Lines.cpp
+++ Uncrossed Lines.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int func(int i,int j,int n,int m,vector<int>& nums1, vector<int>& nums2,vector<vector<int>>& dp){
-        if(i>=n || j>=m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
 
-        if(nums1[i]==nums2[j])
-            return dp[i][j]=1+func(i+1,j+1,n,m,nums1,nums2,dp);
-        else
-            return dp[i][j]=max(func(i,j+1,n,m,nums1,nums2,dp),func(i+1,j,n,m,nums1,nums2,dp));
+    int solve(vector<int>& nums1, vector<int>& nums2, int ind1, int ind2,
+     vector<vector<int>> &dp){
+        if(ind1==nums1.size() or ind2==nums2.size())return 0;
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        if(nums1[ind1]==nums2[ind2]){
+            return dp[ind1][ind2]=1+solve(nums1, nums2, ind1+1, ind2+1, dp);
+        }
+        return dp[ind1][ind2]= max(solve(nums1, nums2, ind1+1, ind2, dp), 
+        solve(nums1, nums2, ind1, ind2+1, dp));
     }
+
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size(),m=nums2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return func(0,0,n,m,nums1,nums2,dp);
+        int n=nums1.size(), m=nums2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solve(nums1, nums2, 0, 0, dp);
     }
 };
